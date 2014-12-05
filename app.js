d132689ee5984111d27808c40b4fe589d86e0f70
Juan Pablo Kutianski <jkutianski@gmail.com>
2014-12-05 01:50:32 -0300

    cleanup

--- app.js
+++ app.js
@@ -1,57 +1,63 @@
 ﻿var schedule = require('node-schedule'),
 	RuleEngine = require('node-rules'),
-	walk = require('walk'),
-	fileRules = walk.walk('./rules/default', {
-		followLinks: false
-	}),
-	fileJobs = walk.walk('./jobs/default', {
-		followLinks: false
-	}),
-	J = false,
-	rules = [],
-	jobs = [],
-	Rules = null;
+	walk = require('walk');
 
-fileJobs.on('file', function(root, stat, next) {
-	if (stat.type == "file" && stat.name.match(/\.json$/gi)) {
-		var thisJob = require(root + '/' + stat.name);
-		if (thisJob && thisJob.schedule) {
-			Object.keys(thisJob.schedule).forEach(function(key) {
-				var item = thisJob.schedule[key];
-				thisJob.schedule[key] = (item != null && item.hasOwnProperty('start')) ? new schedule.Range(parseFloat(item.start), parseFloat(item.end), parseFloat(item.step) || 1) : item;
-			})
+(function (fileJobs, fileRules, RE) {
+	var J = false,
+		jobs = [],
+		rules = [];
+
+	fileJobs.on('file', function(root, stat, next) {
+		if (stat.type == "file" && stat.name.match(/\.json$/gi)) {
+			var thisJob = require(root + '/' + stat.name);
+			if (thisJob && thisJob.schedule) {
+				Object.keys(thisJob.schedule).forEach(function(key) {
+					var item = thisJob.schedule[key];
+					thisJob.schedule[key] = (item != null && item.hasOwnProperty('start')) ? new schedule.Range(parseFloat(item.start), parseFloat(item.end), parseFloat(item.step) || 1) : item;
+				})
+			}
+			jobs[thisJob.id] = thisJob;
 		}
-		jobs[thisJob.id] = thisJob;
-	}
-	next();
-});
-
-fileRules.on('file', function(root, stat, next) {
-	if (stat.type == "file" && stat.name.match(/\.js$/gi)) {
-		var thisRule = require(root + '/' + stat.name);
-		rules.push(thisRule);
-	}
-	next();
-});
-
-fileRules.on('end', function() {
-	Rules = new RuleEngine(rules);
-	if (J) {
-		jobExecution(jobs);
-	}
-});
-
-fileJobs.on('end', function() {
-	J = true;
-	if (Rules) {
-		jobExecution(jobs);
-	}
-});
-
-var jobExecution = function(jobs) {
-	jobs.forEach(function(job) {
-		Rules.execute(job, function(payload) {
-			// console.log(payload)
-		});
+		next();
 	});
-};
+
+	fileRules.on('file', function(root, stat, next) {
+		if (stat.type == "file" && stat.name.match(/\.js$/gi)) {
+			var thisRule = require(root + '/' + stat.name);
+			rules.push(thisRule);
+		}
+		next();
+	});
+
+	fileRules.on('end', function() {
+		RE = new RuleEngine(rules);
+		if (J) {
+			jobExecution(jobs);
+		}
+	});
+
+	fileJobs.on('end', function() {
+		J = true;
+		if (RE) {
+			jobExecution(jobs);
+		}
+	});
+
+	var jobExecution = function(jobs) {
+		jobs.forEach(function(job) {
+			RE.execute(job, function(payload) {
+				// console.log(payload)
+			});
+		});
+	};
+
+})(
+	// var fileJobs
+	walk.walk('./jobs/default', {
+		followLinks: false
+	}),
+	// var fileRules
+	walk.walk('./rules/default', {
+		followLinks: false
+	})
+);
